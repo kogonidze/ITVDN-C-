0a71d30f3c81bca340b19cfbb0321b8e6114f005
kogonidze <kottok1998@gmail.com>
2021-02-27 14:50:12 +0300

    finished second exercise in lesson 1 essential about book

--- Essential/001_Classes/Book/Properties/AssemblyInfo.cs
+++ Essential/001_Classes/Book/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rectangle")]
+[assembly: AssemblyTitle("Book")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rectangle")]
+[assembly: AssemblyProduct("Book")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("60014662-0e56-4bfd-9f02-9c1dee87294a")]
+[assembly: Guid("391ee042-4db0-44c0-bc29-a9f22163b050")]
 
 // Version information for an assembly consists of the following four values:
 //
