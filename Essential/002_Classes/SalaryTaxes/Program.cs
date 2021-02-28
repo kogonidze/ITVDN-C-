2855e0c77477e8d7dfee4c2c35804f4d75eac422
kogonidze <kottok1998@gmail.com>
2021-02-28 23:10:37 +0300

    finished Salary&Taxes app in lesson 2

---
+++ Essential/002_Classes/SalaryTaxes/Program.cs
@@ -0,0 +1,25 @@
+﻿using Shared;
+using System;
+
+namespace SalaryTaxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee JohnAdams = new Employee("Adams", "J.");
+            JohnAdams.Position = Position.Junior;
+            JohnAdams.WorkExperience = 1;
+
+            JohnAdams.PrintInfo();
+
+            Employee AndreyIvanov = new Employee("Ivanov", "A.");
+            AndreyIvanov.Position = Position.Senior;
+            AndreyIvanov.WorkExperience = 6;
+
+            AndreyIvanov.PrintInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
