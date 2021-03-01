eb21596cf803fd8f23f7f7499fdd3f660f1a4ab5
kogonidze <kottok1998@gmail.com>
2021-03-01 22:52:38 +0300

    finished Printer in Lesson 3

---
+++ Essential/003_Inheritance/Printer/Printer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared;
+
+namespace Printer
+{
+    class Printer
+    {
+        public void Print(string value)
+        {
+            FormattingConsoleOutput.ColorsConsoleOutput(value, ConsoleColor.White);
+        }
+    }
+}
---
+++ Essential/003_Inheritance/Printer/YellowPrinter.cs
@@ -0,0 +1,12 @@
+﻿using Shared;
+
+namespace Printer
+{
+    class YellowPrinter : Printer
+    {
+        public void Print(string text)
+        {
+            FormattingConsoleOutput.ColorsConsoleOutput(text, System.ConsoleColor.Yellow);
+        }
+    }
+}
