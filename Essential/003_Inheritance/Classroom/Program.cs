119015717a418556f197a7baf6b099c1c2cb5e0c
kogonidze <kottok1998@gmail.com>
2021-03-02 17:33:03 +0300

    finished Classroom exercise in Lesson 3 essential

---
+++ Essential/003_Inheritance/Classroom/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classroom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classRoom1 = new ClassRoom(new BadPupil(), new GoodPupil(), new BadPupil(), new ExcellentPupil());
+
+            classRoom1.ShowHowPupilsRead();
+            Console.WriteLine(new string('-',50));
+
+            classRoom1.ShowHowPupilsRelax();
+            Console.WriteLine(new string('-', 50));
+
+            var classRoom2 = new ClassRoom(new ExcellentPupil(), new BadPupil());
+            classRoom2.ShowHowPupilsStudy();
+            Console.WriteLine(new string('-', 50));
+            classRoom2.ShowHowPupilsWrite();
+
+            Console.ReadKey();
+        }
+    }
+}
