b04a28488a50bec859de50bd52ce8e16e96f7747
kogonidze <kottok1998@gmail.com>
2021-03-02 18:47:29 +0300

    finished Vehicle exercise in lesson 3 essential

---
+++ Essential/003_Inheritance/Vehicle/Plane.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    class Plane : Vehicle
+    {
+        public double Height { get; set; }
+        public int NumberOfPassengers { get; set; }
+
+        public Plane(double coordX, double coordY, int price, double speed, int productionYear,
+            double height, int numberOfPassengers) : base(coordX, coordY, price, speed, productionYear)
+        {
+            Height = height;
+            NumberOfPassengers = numberOfPassengers;
+        }
+
+        public Plane(int price, double speed, double height, int numberOfPassengers)
+            :base(price, speed)
+        {
+            Height = height;
+            NumberOfPassengers = numberOfPassengers;
+        }
+    }
+}
---
+++ Essential/003_Inheritance/Vehicle/Ship.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    class Ship : Vehicle
+    {
+        public int NumOfPassengers { get; set; }
+        public string HomePort { get; set; }
+
+        public Ship(double coordX, double coordY, int price, double speed, int productionYear, int numOfPassengers, string homePort)
+            : base(coordX, coordY, price, speed, productionYear)
+        {
+            NumOfPassengers = numOfPassengers;
+            HomePort = homePort;
+        }
+
+        public Ship(int price, double speed, int numOfPassengers, string homePort)
+            : base(price, speed)
+        {
+            NumOfPassengers = numOfPassengers;
+            HomePort = homePort;
+        }
+    }
+}
