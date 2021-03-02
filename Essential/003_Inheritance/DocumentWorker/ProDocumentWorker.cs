bccd0cf28cf9452c48d04644f47ac819d49d1742
kogonidze <kottok1998@gmail.com>
2021-03-02 19:15:50 +0300

    finished DocumentWorker exercise in lesson 3 essential

---
+++ Essential/003_Inheritance/DocumentWorker/ProDocumentWorker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentWorker
+{
+    class ProDocumentWorker : DocumentWorker
+    {
+        public override void EditDocument()
+        {
+            Console.WriteLine("Document edited");
+        }
+
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Document saved in old format, saving in another formats is available in Version Expert");
+        }
+    }
+}
