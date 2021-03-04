7d7be7f0b32950d3da8fd4a14902e870d6e6f662
kogonidze <kottok1998@gmail.com>
2021-03-04 10:59:21 +0300

    finished: exercise Document in lesson 4 essential

---
+++ Essential/004_Abstraction/Document/Parts/Header.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Document.Parts
+{
+    class Header : DocumentPart
+    {
+        public override void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(Content);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
