511aada6ceb65ec613e849b4b5b76f978e4062e0
kogonidze <kottok1998@gmail.com>
2021-03-05 18:42:37 +0300

    finished: exercise MyMatrix in lesson 5 essential

---
+++ Essential/005_ArraysAndIndexers/MyMatrix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numOfColumns = Convert.ToInt32(ConfigurationManager.AppSettings["NumberOfColumns"]);
+            var numOfRows = Convert.ToInt32(ConfigurationManager.AppSettings["NumberOfRows"]);
+
+            MyMatrix matrix = new MyMatrix(numOfRows, numOfColumns);
+            matrix.ShowMatrix();
+            matrix.ResizeMatrix(6, 6);
+            matrix.ShowMatrix();
+            matrix.ShowPartOfMatrix(1,4);
+
+            Console.ReadKey();
+        }
+    }
+}
