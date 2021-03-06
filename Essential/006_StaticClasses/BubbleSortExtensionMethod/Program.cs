4d600fb91806305415115c40767459a4a01d4af7
kogonidze <kottok1998@gmail.com>
2021-03-06 18:03:34 +0300

    finished exercise with sorting in extension method lesson 6

---
+++ Essential/006_StaticClasses/BubbleSortExtensionMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSortExtensionMethod
+{
+    class Program
+    {
+        static void InitializeArray(int[] array)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                array[i] = random.Next(1, 100);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var array = new int[20];
+
+            InitializeArray(array);
+
+            var sortedArray = array.BubbleSortAsc();
+
+            Console.ReadKey();
+        }
+    }
+}
