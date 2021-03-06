c722ef00e6bb8d11f9a7639c16c5658c798f2eab
kogonidze <kottok1998@gmail.com>
2021-03-06 16:22:47 +0300

    finished exercise Calculator in lesson 6 essential

---
+++ Essential/006_StaticClasses/Calculator/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    static class Calculator
+    {
+        public static double Sum(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double Substraction(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double Multiplication(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double Division(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public static double Modulo(double num1, double num2)
+        {
+            return num1 % num2;
+        }
+    }
+}
---
+++ Essential/006_StaticClasses/Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.Sum(5.2, 15.1));
+            Console.WriteLine(Calculator.Substraction(15.2, 5.2));
+            Console.WriteLine(Calculator.Multiplication(5, 4));
+            Console.WriteLine(Calculator.Division(20, 2.5));
+            Console.WriteLine(Calculator.Modulo(31.4, 19.2));
+
+            Console.ReadKey();
+        }
+    }
+}
