7488c8546d8588eba31d50641fa4b1afd3b7e965
kogonidze <kottok1998@gmail.com>
2021-03-07 14:25:49 +0300

    finished exercise Train in lesson 7 essential

---
+++ Essential/007_Structures/Train/Dispatcher.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train
+{
+    class Dispatcher
+    {
+        private Train[] trains;
+
+        public Dispatcher()
+        {
+            trains = new Train[8];
+        }
+
+        public string this[int numberTrain]
+        {
+            get
+            {
+                for (int i = 0; i < trains.Length; i++)
+                {
+                    if (trains[i].NumberTrain == numberTrain)
+                    {
+                        return $"Destination: {trains[i].Destination}, Number of train: {trains[i].NumberTrain}, " +
+                            $"Departure time: {trains[i].DepartureTime}";
+                    }
+                }
+
+                return "There is no train with such number!";
+            }
+        }
+
+        public Train[] InitializeTrains()
+        {
+            for (int i = 0; i < trains.Length; i++)
+            {
+                Console.WriteLine("Enter the destination of train: ");
+                var destination = Console.ReadLine();
+
+                Console.WriteLine("Enter the number of train: ");
+                var trainNumber = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the departure time of train: ");
+                var departureTime = TimeSpan.Parse(Console.ReadLine());
+
+                trains[i] = new Train(destination, trainNumber, departureTime);
+            }
+
+            trains = OrderByAscTrains();
+
+            return trains;
+        }
+
+        public Train[] OrderByAscTrains()
+        {
+            for (int i = trains.Length - 1; i >= 0; i--)
+            {
+                for (int j = i-1; j >= 0; j--)
+                {
+                    if (trains[j].NumberTrain > trains[i].NumberTrain)
+                    {
+                        var temp = trains[i];
+                        trains[i] = trains[j];
+                        trains[j] = temp;
+                    }
+                }
+            }
+
+            return trains;
+        }
+
+    }
+}
---
+++ Essential/007_Structures/Train/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dispatcher MinskPassazhirsky = new Dispatcher();
+
+            MinskPassazhirsky.InitializeTrains();
+
+            Console.WriteLine(MinskPassazhirsky[17]);
+            Console.WriteLine(MinskPassazhirsky[1]);
+            Console.WriteLine(MinskPassazhirsky[99]);
+            Console.WriteLine(MinskPassazhirsky[34]);
+            Console.WriteLine(MinskPassazhirsky[13]);
+            Console.WriteLine(MinskPassazhirsky[243]);
+            Console.WriteLine(MinskPassazhirsky[3]);
+            Console.WriteLine(MinskPassazhirsky[7]);
+
+            Console.ReadKey();
+        }
+    }
+}
