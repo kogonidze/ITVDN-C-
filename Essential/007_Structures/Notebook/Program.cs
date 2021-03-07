9b573d13e600762c6ce981f49a271f3491527877
kogonidze <kottok1998@gmail.com>
2021-03-07 12:56:37 +0300

    finished: exercise Notebook in lesson 7 essential

---
+++ Essential/007_Structures/Notebook/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook macBook = new Notebook("Apple", "Macbook Air 2013", 3000);
+            macBook.Show();
+
+            Notebook asusX541 = new Notebook("ASUS", "X541");
+            asusX541.Show();
+
+            Console.WriteLine(macBook.Model);
+            Console.WriteLine(asusX541.Price);
+
+            Console.ReadKey();
+        }
+    }
+}
