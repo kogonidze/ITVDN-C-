c319fd9fe991609aa945d3d6f2c6d16c5c49d95c
kogonidze <kottok1998@gmail.com>
2021-03-07 14:40:06 +0300

    finished: exercise about compare struct and class in lesson 7 essential

---
+++ Essential/007_Structures/CompareStructAndClass/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareStructAndClass
+{
+    class Program
+    {
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "changed";
+        }
+
+        static void StructTaker(MyStruct myStruct)
+        {
+            myStruct.change = "changed";
+        }
+
+        static void Main(string[] args)
+        {
+            var myStruct = new MyStruct();
+            var myClass = new MyClass();
+
+            myClass.change = "not changed";
+            myStruct.change = "not changed";
+
+            Console.WriteLine(myClass.change);
+            Console.WriteLine(myStruct.change);
+
+            ClassTaker(myClass);
+            StructTaker(myStruct);
+
+            Console.WriteLine(myClass.change);
+            Console.WriteLine(myStruct.change);
+
+            Console.ReadKey();
+        }
+    }
+}
