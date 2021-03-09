8a7599e211003e748f200604dd026b0c98a86f36
kogonidze <kottok1998@gmail.com>
2021-03-09 22:49:13 +0300

    finished exercise about working with delegates array in lesson 9 essential

---
+++ Essential/009_Delegates/DelegatesArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesArray
+{
+    class Program
+    {
+        public delegate double AvgDelegate(Delegate[] delegates);
+        public delegate int Delegate();
+
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+
+            Delegate[] randArr = { () => rand.Next(1, 10), () => rand.Next(1,100), () => rand.Next(1,1000)};
+            AvgDelegate avg = (array) =>
+            {
+                var sum = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    sum += array[i]();
+                }
+
+                return (double)sum / array.Length;
+            };
+
+            Console.WriteLine(avg(randArr));
+
+            Console.ReadKey();
+        }
+    }
+}
