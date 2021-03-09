4bd6e03d56e05067d48277f2b7f2a71b72d55608
kogonidze <kottok1998@gmail.com>
2021-03-09 22:37:00 +0300

    finished exercise about creating arithmetic operations in anonymous methods in lesson 9 essential

---
+++ Essential/009_Delegates/ArithmeticOperatorsInAnonymousMethods/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticOperatorsInAnonymousMethods
+{
+    public delegate double ArithmeticDelegate(double x1, double x2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first value: ");
+            var firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second value: ");
+            var secondNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the operation: ");
+            var arithmeticOp = Console.ReadLine();
+
+            ArithmeticDelegate operation = null;
+            switch (arithmeticOp)
+            {
+                case "+":
+                    operation = (x1, x2) => x1 + x2;
+                    break;
+                case "-":
+                    operation = (x1, x2) => x1 - x2;
+                    break;
+                case "*":
+                    operation = (x1, x2) => x1 * x2;
+                    break;
+                case "/":
+                    operation = (x1, x2) =>
+                    {
+                        if (x2 != 0)
+                        {
+                            return x1 / x2;
+                        }
+
+                        Console.WriteLine("You are trying to divide on zero!");
+                        return 0;
+                    };
+
+                    break;
+                default:
+                    Console.WriteLine("You entered incorrect value of operation!");
+                    break;
+            }
+
+            Console.WriteLine($"{operation(firstNum, secondNum)}");
+            Console.ReadKey();
+        }
+    }
+}
