7b55e83c56ac27759139292335b4d8b77ace884d
kogonidze <kottok1998@gmail.com>
2021-03-11 21:39:37 +0300

    finished: exercise about CarCollection in lesson 11 essential

---
+++ Essential/011_Generics(Constraints)/CarCollection/Cars/Bus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarCollection
+{
+    class Bus : BaseCar
+    {
+        public int NumberOfSeats { get; set; }
+
+        public Bus(string name, int yearOfProduction, int numberOfSeats)
+            : base(name, yearOfProduction)
+        {
+            NumberOfSeats = numberOfSeats;
+        }
+    }
+}
---
+++ Essential/011_Generics(Constraints)/CarCollection/Cars/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarCollection
+{
+    class Car : BaseCar
+    {
+        public Car(string name, int yearOfProduction) 
+            : base(name, yearOfProduction)
+        {
+
+        }
+    }
+}
