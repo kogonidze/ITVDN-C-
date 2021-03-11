372ce4679107a409b05046bfcfb6d413d0486af2
kogonidze <kottok1998@gmail.com>
2021-03-11 22:52:53 +0300

    finished: exercise with creating dictionary on lesson 11 essential

---
+++ Essential/011_Generics(Constraints)/Dictionary/Dictionary.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Dictionary<TKey, TValue>
+    {
+        private MyList<TKey> keys;
+        private MyList<TValue> values;
+
+        public Dictionary()
+        {
+            keys = new MyList<TKey>();
+            values = new MyList<TValue>();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return keys.Count;
+            }
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            keys.Add(key);
+            values.Add(value);
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                return $"{keys[index]} : {values[index]}";
+            }
+        }
+
+        public string this[TKey key]
+        {
+            get
+            {
+                var indexOfKey = keys[key];
+                if (indexOfKey != -1)
+                {
+                    return $"{keys[indexOfKey]} : {values[indexOfKey]}";
+                }
+
+                return "There is no such element in dictionary!";
+            }
+        }
+
+        public bool Contains(TKey key)
+        {
+            return keys.Contains(key);
+        }
+
+        public void Clear()
+        {
+            keys.Clear();
+            values.Clear();
+        }
+    }
+}
---
+++ Essential/011_Generics(Constraints)/Dictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> engToRus = new Dictionary<string, string>();
+            engToRus.Add("Book", "книга");
+            engToRus.Add("Table", "стол");
+            engToRus.Add("Bike", "велосипед");
+
+            Console.WriteLine(engToRus.Count);
+            Console.WriteLine(engToRus[1]);
+            Console.WriteLine(engToRus["Table"]);
+            Console.WriteLine(engToRus["SUN"]);
+            Console.WriteLine(engToRus.Contains("Bike"));
+
+            engToRus.Clear();
+
+            Console.WriteLine(engToRus.Count);
+
+            Console.ReadKey();
+        }
+    }
+}
