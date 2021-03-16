bea5274775671c0a6de4af69a2b7822fe8d8450b
kogonidze <kottok1998@gmail.com>
2021-03-16 20:06:32 +0300

    finished: program with recursive function with multithreading in lesson 13 essential

---
+++ Essential/013_Threads/RecursiveFunctionThreads/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RecursiveFunctionThreads
+{
+    class Program
+    {
+        static void RecursiveFunc()
+        {
+            var thread = Thread.CurrentThread;
+
+            Console.WriteLine($"ID of thread: {thread.GetHashCode()}");
+            Thread.Sleep(500);
+            new Thread(RecursiveFunc).Start();
+
+        }
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(RecursiveFunc);
+            thread.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
