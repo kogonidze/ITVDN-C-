e8866b2ee8ce8e07dfe7fffbd956586a71f56ed2
kogonidze <kottok1998@gmail.com>
2021-03-20 00:02:43 +0300

    refactored code in fallingLetters application, implemented singleton pattern

---
+++ Essential/013_Threads/FallingLetters/Helpers/RandomGeneratorHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FallingLetters.Helpers
+{
+    public abstract class RandomGeneratorHelper
+    {
+        public static Random rand = new Random();
+        public static char GenerateLetter()
+        {
+            return (char)rand.Next(97, 122);
+        }
+
+        public static int[] GenerateChainSizes()
+        {
+            var arrSizes = new int[Console.WindowWidth];
+
+            for (int i = 0; i < arrSizes.Length; i++)
+            {
+                arrSizes[i] = rand.Next(3, 10);
+            }
+
+            return arrSizes;
+        }
+
+        public static int[] GenerateInitialSteps(int length)
+        {
+            var initialSteps = new int[Console.WindowWidth];
+
+            for (int i = 0; i < length; i++)
+            {
+                initialSteps[i] = rand.Next(1, 20);
+            }
+
+            return initialSteps;
+        }
+    }
+}
---
+++ Essential/013_Threads/FallingLetters/Matrix.cs
@@ -0,0 +1,157 @@
+﻿using FallingLetters.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FallingLetters
+{
+    public class Matrix
+    {
+        public char[,] array;
+        public int width;
+        public int height;
+        public int[] arrSizes;
+        public int[] initialSteps;
+
+        private static Matrix matrix;
+
+        private Matrix()
+        {
+            width = Console.WindowWidth;
+            height = Console.WindowHeight;
+            array = new char[height, width];
+            arrSizes = RandomGeneratorHelper.GenerateChainSizes();
+        }
+
+        public static Matrix GetMatrix()
+        {
+            if (matrix == null)
+            {
+                matrix = new Matrix();
+                matrix.ConfigureMatrix();
+            }
+
+            return matrix;
+        }
+
+        void ConfigureMatrix()
+        {
+            matrix.initialSteps = RandomGeneratorHelper.GenerateInitialSteps(matrix.arrSizes.Length);
+            InitializeMatrix(matrix.array);
+            PrintMatrix(matrix.array);
+        }
+
+        static void InitializeMatrix(char[,] array)
+        {
+            for (int i = 0; i < matrix.height; i++)
+            {
+                for (int j = 0; j < matrix.width; j++)
+                {
+                    if (i < matrix.arrSizes[j])
+                    {
+                        array[i + matrix.initialSteps[j], j] = RandomGeneratorHelper.GenerateLetter();
+                    }
+                }
+            }
+        }
+
+        static int[] IncrementInitialSteps()
+        {
+            var newInitialSteps = new int[Console.WindowWidth];
+
+            for (int i = 0; i < matrix.initialSteps.Length; i++)
+            {
+                if (matrix.initialSteps[i] + 1 == matrix.height)
+                {
+                    newInitialSteps[i] = (matrix.initialSteps[i] + 1) % 30;
+                }
+                else
+                {
+                    newInitialSteps[i] = matrix.initialSteps[i] + 1;
+                }
+
+            }
+
+            matrix.initialSteps = newInitialSteps;
+
+            return matrix.initialSteps;
+        }
+
+        public static void MoveLetters(object obj)
+        {
+            var step = (int[])obj;
+
+            Console.Clear();
+            char[,] array = new char[matrix.height, matrix.width];
+
+            for (int i = 0; i < matrix.height; i++)
+            {
+                for (int j = 0; j < matrix.width; j++)
+                {
+                    if (i + 1 < matrix.arrSizes[j])
+                    {
+                        if (i + step[j] < matrix.height)
+                        {
+                            array[i + step[j], j] = RandomGeneratorHelper.GenerateLetter();
+                        }
+                        else if (i + step[j] % matrix.height == matrix.height)
+                        {
+                            array[0, j] = RandomGeneratorHelper.GenerateLetter();
+                        }
+                        else
+                        {
+                            array[i + (step[j] - matrix.height), j] = RandomGeneratorHelper.GenerateLetter();
+                        }
+                    }
+                }
+            }
+
+            obj = IncrementInitialSteps();
+
+            PrintMatrix(array);
+        }
+
+        static void PrintMatrix(char[,] array)
+        {
+            for (int i = 0; i < matrix.height; i++)
+            {
+                for (int j = 0; j < matrix.width; j++)
+                {
+                    if (CheckIfFirstSymbolInChain(array, i, j))
+                    {
+                        lock (Program.syncRoot)
+                        {
+                            FormattingOutputHelper.ColouredOutputInConsole(array[i, j], ConsoleColor.White);
+                        }
+                    }
+                    else if (CheckIfSecondSymbolInChain(array, i, j))
+                    {
+                        lock (Program.syncRoot)
+                        {
+                            FormattingOutputHelper.ColouredOutputInConsole(array[i, j], ConsoleColor.Green);
+                        }
+                    }
+                    else
+                    {
+                        lock (Program.syncRoot)
+                        {
+                            FormattingOutputHelper.ColouredOutputInConsole(array[i, j], ConsoleColor.DarkGreen);
+                        }
+                    }
+                }
+            }
+        }
+
+        static bool CheckIfFirstSymbolInChain(char[,] array, int i, int j)
+        {
+            return array[(i + 1) % matrix.height, j] == 0;
+        }
+
+        static bool CheckIfSecondSymbolInChain(char[,] array, int i, int j)
+        {
+            return array[(i + 2) % matrix.height, j] == 0 && array[(i + 1) % matrix.height, j] != 0;
+        }
+    }
+}
