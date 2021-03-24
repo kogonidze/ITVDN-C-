bb3bd80d072c9475c7aff34ef2f6351cce51b7cb
kogonidze <kottok1998@gmail.com>
2021-03-24 17:14:04 +0300

    finished House app about surface cloning and deep cloning in lesson 16 essential

---
+++ Essential/016_Operators/House/Material.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace House
+{
+    class Material : ICloneable
+    {
+        public string Walls { get; set; }
+        public string Roof { get; set; }
+
+        public Material(string walls, string roof)
+        {
+            Walls = walls;
+            Roof = roof;
+        }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+
+        public override string ToString()
+        {
+            return $"Walls: {Walls} Roof: {Roof}";
+        }
+    }
+}
---
+++ Essential/016_Operators/House/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace House
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Material brickTiles = new Material("brick", "tiles");
+            var house = new House(brickTiles, 10000);
+
+            Console.WriteLine($"Surface cloning: ");
+            var house2 = house.Clone();
+
+            Console.WriteLine(house);
+            Console.WriteLine(house2);
+            Console.WriteLine(new string('-', 50));
+
+            house2.Price = 20000;
+            house2.Material.Roof = "slate";
+
+            Console.WriteLine(house);
+            Console.WriteLine(house2);
+            Console.WriteLine(new string('-', 50));
+
+            Console.WriteLine($"Deep cloning: ");
+            var house3 = house.DeepClone();
+
+            Console.WriteLine(house);
+            Console.WriteLine(house3);
+            Console.WriteLine(new string('-', 50));
+
+            house3.Price = 10000;
+            house3.Material.Roof = "brick";
+
+            Console.WriteLine(house);
+            Console.WriteLine(house3);
+            Console.WriteLine(new string('-', 50));
+
+            Console.ReadKey();
+        }
+    }
+}
