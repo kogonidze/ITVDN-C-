f674f2f26b96ca73598218e680a3bac1135e66a5
kogonidze <kottok1998@gmail.com>
2021-03-24 14:23:02 +0300

    finished Point3D app in lesson 16 essential

---
+++ Essential/016_Operators/Point3D/Point.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point3D
+{
+    public struct Point
+    {
+        private double x, y, z;
+
+        public Point(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Point operator +(Point p1, Point p2)
+        {
+            return new Point(p1.x + p2.x, p1.y + p2.y, p1.z + p2.z);
+        }
+
+        public static Point operator -(Point p1, Point p2)
+        {
+            return new Point(p1.x - p2.x, p1.y - p2.y, p1.z - p2.z);
+        }
+
+        public static Point operator *(Point p1, Point p2)
+        {
+            return new Point(p1.x * p2.x, p1.y * p2.y, p1.z * p2.z);
+        }
+
+        public static Point operator /(Point p1, Point p2)
+        {
+            return new Point(p1.x / p2.x, p1.y / p2.y, p1.z / p2.z);
+        }
+
+        public override string ToString()
+        {
+            return $"X: {x} Y: {y} Z:{z}";
+        }
+    }
+}
