a53596b773c279a08453662327cdda4ff9d318bf
kogonidze <kottok1998@gmail.com>
2021-03-29 15:06:27 +0300

    created structure of citizens classes in CollectionOfCitizens app, les.1 prof.

---
+++ Professional/001_UserCollections/CollectionOfCitizens/Citizens/Citizen.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionOfCitizens.Citizens
+{
+    abstract class Citizen
+    {
+        public int PassportID { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public int YearOfBirth { get; } 
+
+        public Citizen(int passportID, string firstName, string lastName, int yearOfBirth)
+        {
+            PassportID = passportID;
+            FirstName = firstName;
+            LastName = lastName;
+            YearOfBirth = yearOfBirth;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Citizen))
+            {
+                return false;
+            }
+
+            var citizen = obj as Citizen;
+
+            if (this.PassportID == citizen.PassportID)
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public override int GetHashCode()
+        {
+            return PassportID;
+        }
+
+        public static bool operator ==(Citizen firstCitizen, Citizen secondCitizen)
+        {
+            if (firstCitizen.Equals(secondCitizen))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Citizen firstCitizen, Citizen secondCitizen)
+        {
+            if (firstCitizen.Equals(secondCitizen))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Professional/001_UserCollections/CollectionOfCitizens/Citizens/Retiree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionOfCitizens.Citizens
+{
+    class Retiree : Citizen
+    {
+        public Retiree(int passportID, string firstName, string lastName, int yearOfBirth)
+            : base(passportID, firstName, lastName, yearOfBirth)
+        {
+
+        }
+    }
+}
