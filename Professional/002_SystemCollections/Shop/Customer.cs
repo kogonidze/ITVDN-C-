7daf1e3ca398ae1381b7bf729ae19532596afb01
kogonidze <kottok1998@gmail.com>
2021-03-31 16:52:42 +0300

    finished exercise about shopCollection in lesson 2 professional

---
+++ Professional/002_SystemCollections/Shop/Customer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PassportID { get; set; }
+
+        public Customer(string firstName, string lastName, int passportID)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PassportID = passportID;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var customer = obj as Customer;
+
+            if (customer == null)
+            {
+                return false;
+            }
+
+            return customer.FirstName == FirstName && 
+                customer.LastName == LastName &&
+                customer.PassportID == PassportID;
+        }
+
+        public override int GetHashCode()
+        {
+            return PassportID.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
---
+++ Professional/002_SystemCollections/Shop/ShopCollectionEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    public class ShopCollectionEntry
+    {
+        public Customer Customer { get; }
+        public ProductCategory ProductCategory { get; }
+
+        public ShopCollectionEntry(Customer customer, ProductCategory productCategory)
+        {
+            Customer = customer;
+            ProductCategory = productCategory;
+        }
+    }
+}
