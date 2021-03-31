1d88ffa247db76afdb738ba9c7fe204076c3011a
kogonidze <kottok1998@gmail.com>
2021-03-31 17:51:30 +0300

    began ordered dictionary in lesson 2 professional

---
+++ Professional/002_SystemCollections/OrderedDictionary/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithOrderedDictionary
+{
+    class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PassportID { get; set; }
+
+        public Customer(string firstName, string lastName, int passportID)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PassportID = passportID;
+        }
+    }
+}
---
+++ Professional/002_SystemCollections/OrderedDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithOrderedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var orderedDict = new OrderedDictionary(new MyEqualityComparer());
+
+            orderedDict.Add(new Customer("Ivan", "Popov", 1), 1);
+            orderedDict.Add(new Customer("Viktor", "Viktorov", 2), 2);
+            orderedDict.Add(new Customer("Viktor", "Viktorov", 2), 3);
+
+            Console.WriteLine(orderedDict.Contains(new Customer("Viktor", "Viktorov", 2)));
+
+            Console.ReadKey();
+        }
+    }
+}
