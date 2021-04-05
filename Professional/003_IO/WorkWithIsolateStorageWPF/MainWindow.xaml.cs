1afa11114e2f5f79759589b6f23af2ef537c46a7
kogonidze <kottok1998@gmail.com>
2021-04-05 17:42:05 +0300

    implemented main logic in WorkWithIsolateStorageWPF app in less 3 prof

--- Professional/003_IO/WorkWithIsolateStorageWPF/MainWindow.xaml.cs
+++ Professional/003_IO/WorkWithIsolateStorageWPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,50 @@ namespace WorkWithIsolateStorageWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        IsolatedStorageFile userStorage;
+        IsolatedStorageFileStream userStream;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            userStorage = IsolatedStorageFile.GetUserStoreForAssembly();
+            userStream =
+                new IsolatedStorageFileStream("ColorSettings.dat", FileMode.OpenOrCreate, userStorage);
+
+            using (var streamReader = new StreamReader(userStream))
+            {
+                var contents = streamReader.ReadToEnd();
+                if (!string.IsNullOrEmpty(contents))
+                {
+                    ColorPicker.SelectedColor = (Color?)ColorConverter.ConvertFromString(contents);
+                    ColorLabel.Content = contents;
+                    ColorLabel.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(contents));
+                }               
+            }
+        }
+
+        private void ColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            ColorLabel.Content = e.NewValue.Value;
+            ColorLabel.Background = new SolidColorBrush(e.NewValue.Value);
+        }
+
+        private void SaveDataBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //IsolatedStorageFile userStorage = IsolatedStorageFile.GetUserStoreForAssembly();
+            //IsolatedStorageFileStream userStream = 
+            //    new IsolatedStorageFileStream("ColorSettings.dat", System.IO.FileMode.Create, userStorage);
+            userStream =
+               new IsolatedStorageFileStream("ColorSettings.dat", System.IO.FileMode.Create, userStorage);
+
+            using (var userWriter = new StreamWriter(userStream))
+            {
+                userWriter.WriteLine(ColorPicker.SelectedColor);
+            }
         }
     }
 }
