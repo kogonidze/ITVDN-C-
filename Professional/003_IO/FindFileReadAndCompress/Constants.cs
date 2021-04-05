4c185003c29c645b2b18d6320dc777b47d207a98
kogonidze <kottok1998@gmail.com>
2021-04-05 12:43:24 +0300

    finished FindFileReadAndCompress app in lesson 3 professional

---
+++ Professional/003_IO/FindFileReadAndCompress/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindFileReadAndCompress
+{
+    abstract class Constants
+    {
+        public const string Yes = "Y";
+
+        public const string FinishedMsg = "Finished!";
+
+        public const string FileNotFoundMsg = "File not found!";
+
+        public const string AskIfCompress = "Do you want to compress this file (Y/N)?";
+    }
+}
---
+++ Professional/003_IO/FindFileReadAndCompress/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindFileReadAndCompress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFilePath = ConfigurationManager.AppSettings["InputFilePath"];
+            var compressedFilePath = ConfigurationManager.AppSettings["CompressedFilePath"];
+
+            if (CheckIfFileExists(inputFilePath))
+            {
+                OpenFileInNotepad(inputFilePath);
+
+                Console.WriteLine(Constants.AskIfCompress);
+
+                var answer = Console.ReadLine();
+
+                if (answer == Constants.Yes)
+                {
+                    CompressFile(inputFilePath, compressedFilePath);
+                }
+            }
+
+            Console.WriteLine(Constants.FinishedMsg);
+
+            Console.ReadKey();
+        }
+
+        static void OpenFileInNotepad(string inputFilePath)
+        {
+            var process = new Process();
+            process.StartInfo = new ProcessStartInfo()
+            {
+                FileName = inputFilePath
+            };
+
+            process.Start();
+            process.WaitForExit();
+        }
+
+        static bool CheckIfFileExists(string inputFilePath)
+        {
+            try
+            {
+                using (var fs = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    return true;
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine(Constants.FileNotFoundMsg);
+            }
+
+            return false;
+        }
+
+        static void CompressFile(string inputFilePath, string compressedFilePath)
+        {
+            using (var sourceStream = new FileStream(inputFilePath, FileMode.OpenOrCreate))
+            {
+                using (var destinationStream = new FileStream(compressedFilePath, FileMode.Create))
+                {
+                    using (var compressionStream = new GZipStream(destinationStream, CompressionMode.Compress))
+                    {
+                        sourceStream.CopyTo(compressionStream);
+                    }
+                }
+            }
+        }
+    }
+}
