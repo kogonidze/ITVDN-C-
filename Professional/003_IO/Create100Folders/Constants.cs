e4ac13b788c876e6c3ae2f431f825520059ebea4
kogonidze <kottok1998@gmail.com>
2021-04-05 10:26:43 +0300

    finished exercise Create100Folders in lesson 3 professional

---
+++ Professional/003_IO/Create100Folders/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create100Folders
+{
+    abstract class Constants
+    {
+        public const string PathRoot = @"C:\";
+
+        public const string ExampleDirName = "Example";
+
+        public const string PermanentPartOfDirName = "Folder_";
+
+        public const string FinishedMsg = "Finished!";
+
+        public const int CountOfDir = 100;
+    }
+}
