73c8af3e0e8bc2fcd36347da897cda2761859f5f
kogonidze <kottok1998@gmail.com>
2021-04-08 12:28:16 +0300

    finished LoginPassword exercise in lesson 4 professional

---
+++ Professional/004_RegularExpressions/LoginPassword/Program.cs
@@ -0,0 +1,55 @@
+﻿using LoginPassword.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LoginPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(MessagesConstants.EnterLoginMsg);
+                var login = Console.ReadLine();
+
+                if (!FieldValidation(login, PatternConstants.patternForLogin))
+                {
+                    Console.WriteLine(MessagesConstants.IncorrectSymbolsErrorMsg);
+                    break;
+                }
+
+                Console.WriteLine(MessagesConstants.EnterPasswordMsg);
+                var passwd = Console.ReadLine();
+
+                if (!FieldValidation(passwd, PatternConstants.patternForPassword))
+                {
+                    Console.WriteLine(MessagesConstants.IncorrectSymbolsErrorMsg);
+                    break;
+                }
+
+                Console.WriteLine(MessagesConstants.SuccessfullRegistrationMsg);
+                Thread.Sleep(3000);
+
+                Console.Clear();
+            }
+
+            Console.ReadKey();
+        }
+
+        static bool FieldValidation(string input, string pattern)
+        {
+            if (Regex.IsMatch(input, pattern))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
