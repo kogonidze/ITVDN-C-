022f5ebd5072359c59fdfb3ce88bba52115bb451
kogonidze <kottok1998@gmail.com>
2021-04-24 14:55:44 +0300

    finished Decoder app in lesson 4 professional

---
+++ Professional/004_RegularExpressions/Decoder/PatternConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decoder
+{
+    abstract class PatternConstants
+    {
+        public const string PrepositionPattern = @"( [пП]од | [Нн]а | [вВ] | [зЗ]а | [кК] | [иИ]з | [пП]о | [оО]б | [оО]т | [уУ] | [сС] |" + 
+                                                @" [оО] | [оО]коло | [пП]ри | [пП]еред | [бБ]ез | [Мм]ежду | [Вв]округ | [Дд]ля | [Пп]ро } |" +
+                                                @" [иИ]з-за | [иИ]з-под | [дД]о )";
+    }
+}
---
+++ Professional/004_RegularExpressions/Decoder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Decoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pathToExampleFolder = AppDomain.CurrentDomain.BaseDirectory;
+            var inputFileName = ConfigurationManager.AppSettings["InputFileName"];
+            var outputFileName = ConfigurationManager.AppSettings["OutputFileName"];
+
+            var text = File.ReadAllText(inputFileName);
+            text = Regex.Replace(text, PatternConstants.PrepositionPattern, " ГАВ! ");
+            File.WriteAllText(outputFileName, text);
+
+            Console.ReadKey();
+        }
+    }
+}
