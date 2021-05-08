af4dfd81b068563b880c8b74d85dd5e1cfe31382
kogonidze <kottok1998@gmail.com>
2021-05-08 20:29:52 +0300

    created ClientApp in Registry app; implemented reading from Windows registry

---
+++ Professional/005_XML/Registry/ClientApp/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace ClientApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.welcomeMessage_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // welcomeMessage_label
+            // 
+            this.welcomeMessage_label.AutoSize = true;
+            this.welcomeMessage_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeMessage_label.Location = new System.Drawing.Point(101, 48);
+            this.welcomeMessage_label.Name = "welcomeMessage_label";
+            this.welcomeMessage_label.Size = new System.Drawing.Size(0, 55);
+            this.welcomeMessage_label.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.welcomeMessage_label);
+            this.Name = "Form1";
+            this.Text = "ClientApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label welcomeMessage_label;
+    }
+}
+
