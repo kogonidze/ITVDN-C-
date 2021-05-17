4e42159a7114f4a8579c6953f08802cb6e8e8bfd
kogonidze <kottok1998@gmail.com>
2021-05-17 13:19:44 +0300

    + created auxiliary assembly Temperature in lesson 6 professional

---
+++ Professional/006_Reflection/Temperature/Temperature.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperature
+{
+    public class Temperature : IFormattable
+    {
+        private decimal temperature;
+
+        public Temperature(decimal temperature)
+        {
+            if (temperature < -273.15m)
+            {
+                throw new ArgumentOutOfRangeException(
+                    String.Format("{0} is less than absolute zero.", temperature));
+            }
+
+            this.temperature = temperature;
+        }
+
+        public decimal Celsius
+        {
+            get
+            {
+                return temperature;
+            }
+        }
+
+        public decimal Fahrenheit
+        {
+            get
+            {
+                return temperature * 9 / 5 + 32;
+            }
+        }
+
+        public decimal Kelvin
+        {
+            get
+            {
+                return temperature + 273.15m;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.ToString("G", CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (String.IsNullOrEmpty(format))
+                format = "G";
+
+            if (formatProvider == null)
+                formatProvider = CultureInfo.CurrentCulture;
+
+            switch (format.ToUpperInvariant())
+            {
+                case "G":
+                case "C":
+                    return temperature.ToString("F2", formatProvider) + "C";
+                case "F":
+                    return Fahrenheit.ToString("F2", formatProvider) + "F";
+                case "K":
+                    return Kelvin.ToString("F2", formatProvider) + "K";
+                default:
+                    throw new FormatException(
+                        String.Format("The {0} format string is not supported.", format));
+            }
+        }
+    }
+}
