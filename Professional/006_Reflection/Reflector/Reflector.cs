1395e280a9821bb7d1c1311096e81a9be6f34c58
kogonidze <kottok1998@gmail.com>
2021-05-21 15:23:44 +0300

    + finished Reflector app in lesson 6 professional

--- Professional/006_Reflection/Reflector/Reflector.cs
+++ Professional/006_Reflection/Reflector/Reflector.cs
@@ -80,23 +80,19 @@ namespace ReflectorNS
                 switch (member.MemberType)
                 {
                     case MemberTypes.Constructor:
+                        PrintConstructorInfo(member as ConstructorInfo);
                         break;
                     case MemberTypes.Event:
+                        PrintEventInfo(member as EventInfo);
                         break;
                     case MemberTypes.Field:
+                        PrintFieldInfo(member as FieldInfo);
                         break;
                     case MemberTypes.Method:
                         PrintMethodInfo(member as MethodInfo);
                         break;
                     case MemberTypes.Property:
-                        break;
-                    case MemberTypes.TypeInfo:
-                        break;
-                    case MemberTypes.Custom:
-                        break;
-                    case MemberTypes.NestedType:
-                        break;
-                    case MemberTypes.All:
+                        PrintPropertyInfo(member as PropertyInfo);
                         break;
                     default:
                         break;
@@ -116,6 +112,77 @@ namespace ReflectorNS
             Console.WriteLine($"Is virtual: {method.IsVirtual}");
             Console.WriteLine($"Declaring type: {method.DeclaringType}");
             Console.WriteLine($"Calling convention: {method.CallingConvention}");
+            Console.WriteLine();
+
+            PrintParametersInfo(method);
+
+
+            Console.WriteLine();
+        }
+
+        private static void PrintParametersInfo(MethodInfo method)
+        {
+            var parameters = method.GetParameters();
+
+            Console.WriteLine($"Method has {parameters.Length} parameters");
+
+            foreach (var parameter in parameters)
+            {
+                Console.WriteLine($"Name: {parameter.Name}");
+                Console.WriteLine($"Position: {parameter.Position}");
+                Console.WriteLine($"Parameter type: {parameter.ParameterType}");
+                Console.WriteLine($"Is in: {parameter.IsIn}");
+                Console.WriteLine($"Is out: {parameter.IsOut}");
+                Console.WriteLine($"Is return value: {parameter.IsRetval}");
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void PrintFieldInfo(FieldInfo field)
+        {
+            Console.WriteLine($"Info about field {field.Name} with field type {field.FieldType}");
+
+            Console.WriteLine($"Is private: {field.IsPrivate}");
+            Console.WriteLine($"Is public: {field.IsPublic}");
+            Console.WriteLine($"Is static: {field.IsStatic}");
+            Console.WriteLine($"Is literal: {field.IsLiteral}");
+            Console.WriteLine($"Declaring type: {field.DeclaringType}");
+            Console.WriteLine($"Member type: {field.MemberType}");
+
+            Console.WriteLine();
+        }
+
+        private static void PrintPropertyInfo(PropertyInfo property)
+        {
+            Console.WriteLine($"Info about property {property.Name} with field type {property.PropertyType}");
+
+            Console.WriteLine($"Can read: {property.CanRead}");
+            Console.WriteLine($"Can write: {property.CanWrite}");
+            Console.WriteLine($"Get method: {property.GetMethod}");
+            Console.WriteLine($"Set method: {property.SetMethod}");
+
+            Console.WriteLine();
+        }
+
+        private static void PrintConstructorInfo(ConstructorInfo constructor)
+        {
+            Console.WriteLine($"Info about constructor {constructor.Name}");
+
+            Console.WriteLine($"Is public: {constructor.IsPublic}");
+            Console.WriteLine($"Is static: {constructor.IsStatic}");
+            Console.WriteLine($"Is private: {constructor.IsPrivate}");
+            Console.WriteLine($"Is hide by signature: {constructor.IsHideBySig}");
+            Console.WriteLine($"Declaring type: {constructor.DeclaringType}");
+            
+
+            Console.WriteLine();
+        }
+
+        private static void PrintEventInfo(EventInfo @event)
+        {
+            Console.WriteLine($"Info about event {@event.Name}");
+            Console.WriteLine($"Event handler type: {@event.EventHandlerType}");
 
             Console.WriteLine();
         }
