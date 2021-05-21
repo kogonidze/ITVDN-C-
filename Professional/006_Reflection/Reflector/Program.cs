a66ba9899896663deafab9efc9c9c97ec8094d2c
kogonidze <kottok1998@gmail.com>
2021-05-21 14:23:59 +0300

    + implemented loading assembly and printing assembly info in Reflector app lesson 6 professional

--- Professional/006_Reflection/Reflector/Program.cs
+++ Professional/006_Reflection/Reflector/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Reflector
+namespace ReflectorNS
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var assemblyFile = ConfigurationManager.AppSettings["AssemblyFile"];
+
+            var assembly = Reflector.LoadAssembly(assemblyFile);
+
+            Reflector.PrintAssemblyInfo(assembly);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Professional/006_Reflection/Reflector/Reflector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectorNS
+{
+    public abstract class Reflector
+    {
+        internal static Assembly LoadAssembly(string assemblyFile)
+        {
+            try
+            {
+                var assembly = Assembly.LoadFrom(assemblyFile);
+                Console.WriteLine($"Assembly `{assembly.FullName}` was successfully loaded!");
+                Console.WriteLine();
+
+                return assembly;
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        internal static void PrintAssemblyInfo(Assembly assembly)
+        {
+            Console.WriteLine($"Info about assembly `{assembly}`: ");
+            Console.WriteLine($"Manifest module: {assembly.ManifestModule}");
+            Console.WriteLine($"Full name: {assembly.FullName}");
+            Console.WriteLine($"Location: {assembly.Location}");
+            Console.WriteLine($"ImageRuntimeVersion: {assembly.ImageRuntimeVersion}");
+            Console.WriteLine($"Is dynamic: {assembly.IsDynamic}");
+            Console.WriteLine($"Is fully trusted: {assembly.IsFullyTrusted}");
+
+            Console.WriteLine("Attributes: ");
+            foreach (var item in assembly.CustomAttributes)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
