f0ea642df93747b27dce5ce3d54cf49e2d314ef8
kogonidze <kottok1998@gmail.com>
2023-04-06 20:10:05 +0300

    refactor(essentialLesson1): reimplementation of Address exercise

---
+++ Essential/Address/Address.cs
@@ -0,0 +1,54 @@
+﻿namespace AddressExercise;
+
+public class Address
+{
+    private string index;
+    private string country;
+    private string city;
+    private string street;
+    private string house;
+    private string apartment;
+
+    public string Index 
+    {
+        get { return index; }
+        set 
+        { 
+            if (value != "Foo") index = value;
+        }
+    }
+
+    public string Country 
+    { 
+        get { return country; }
+        set { country = value; }
+    }
+
+    public string City
+    {
+        get { return city; }
+        set { city = value; }
+    }
+
+    public string Street
+    {
+        get { return street; }
+        set { street = value; }
+    }
+
+    public string House
+    {
+        get { return house; }
+        set { house = value; }
+    }
+
+    public string Apartment
+    {
+        get { return apartment; }
+        set { apartment = value; }
+    }
+
+    public string ShowInfo()
+        => $"Index: {index}, country: {country}, city: {city}," +
+        $" street: {street}, house: {house}, apartment: {apartment}";
+}
---
+++ Essential/Address/Program.cs
@@ -0,0 +1,14 @@
+﻿using AddressExercise;
+
+var address = new Address();
+
+address.Index = "12345";
+address.Country = "Belarus";
+address.City = "Minsk";
+address.Street = "Kudykina";
+address.House = "1";
+address.Apartment = "1";
+
+Console.WriteLine(address.ShowInfo());
+
+Console.ReadKey();
