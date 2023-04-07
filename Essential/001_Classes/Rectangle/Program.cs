0c50460c44a20ff441170803d4c5f3679bcf5134
kogonidze <kottok1998@gmail.com>
2023-04-07 21:29:03 +0300

    refactor(essentialLesson1): reimplementation of Rectangle exercise

---
+++ Essential/001_Classes/Rectangle/Program.cs
@@ -0,0 +1,22 @@
+﻿using RectangleExercise;
+
+Console.WriteLine("Enter side1 value: ");
+
+if (!double.TryParse(Console.ReadLine(), out double side1))
+{
+    Console.WriteLine("Side1 must be a double value!");
+    Environment.Exit(-1);
+}
+
+Console.WriteLine("Enter side2 value: ");
+
+if (!double.TryParse(Console.ReadLine(), out double side2))
+{
+    Console.WriteLine("Side2 must be a double value!");
+    Environment.Exit(-1);
+}
+
+var rectangle = new Rectangle(side1, side2);
+Console.WriteLine(rectangle.ShowInfo());
+
+Console.ReadKey();
