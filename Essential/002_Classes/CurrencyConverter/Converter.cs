241b625d2bea00f8de5891c1a0f9d26d7455658c
kogonidze <kottok1998@gmail.com>
2023-04-08 12:21:29 +0300

    refactor(essentialLesson2): reimplementation of CurrencyConverter exercise

--- Essential/002_Classes/CurrencyConverter/Converter.cs
+++ Essential/002_Classes/CurrencyConverter/Converter.cs
@@ -1,65 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace CurrencyConverter;
 
-namespace CurrencyConverter
+internal class Converter
 {
-    enum Currency
+    private double usd;
+    private double eur;
+    private double rub;
+
+    public Converter(double usd, double eur, double rub)
     {
-        USD,
-        EUR,
-        RUB,
+        this.usd = usd;
+        this.eur = eur;
+        this.rub = rub;
     }
-    class Converter
+
+    public double SoldBYN(double amount, Currency target)
     {
-        public double USD { get; set; }
-        public double EUR { get; set; }
-        public double RUB { get; set; }
-
-        public Converter(double usd, double eur, double rub)
+        return target switch
         {
-            USD = usd;
-            EUR = eur;
-            RUB = rub;
-        }
+            Currency.USD => amount / usd,
+            Currency.EUR => amount / eur,
+            Currency.RUB => amount / rub,
+            _ => throw new InvalidOperationException("Not supported target currency!"),
+        };
+    }
 
-        public double GetCurrencyRate(Currency currency)
+    public double BuyBYN(double amount, Currency source) 
+    {
+        return source switch
         {
-            switch (currency)
-            {
-                case Currency.USD:
-                    return USD;
-                case Currency.EUR:
-                    return EUR;
-                case Currency.RUB:
-                    return RUB;
-            }
-
-            return 0.0;
-        }
-        public void Convert(Currency currency, bool isConvertToUAH)
-        {
-            Console.WriteLine("Enter the amount of money: ");
-
-            if (double.TryParse(Console.ReadLine(), out double amount))
-            {
-                var currencyRate = GetCurrencyRate(currency);
-
-                if (isConvertToUAH)
-                {
-                    Console.WriteLine($"Now you have {amount * currencyRate} UAH");
-                }
-                else
-                {
-                    Console.WriteLine($"Now you have {amount / currencyRate} {currency}");
-                }
-            }
-            else
-            {
-                Console.WriteLine("You entered incorrect value");
-            } 
-        }
+            Currency.USD => amount * usd,
+            Currency.EUR => amount * eur,
+            Currency.RUB => amount * rub,
+            _ => throw new InvalidOperationException("Not supported source currency!"),
+        };
     }
 }
