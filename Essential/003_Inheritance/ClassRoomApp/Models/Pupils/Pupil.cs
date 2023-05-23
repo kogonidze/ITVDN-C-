a60eb527ccbc5bb12740dab0fddf79a9cfbfbbe3
kogonidze <kottok1998@gmail.com>
2023-05-23 21:51:24 +0300

    refactor(essentialLesson3): reimplementation of ClassRoom exercise

---
+++ Essential/003_Inheritance/ClassRoomApp/Models/Pupils/Pupil.cs
@@ -0,0 +1,24 @@
+﻿namespace ClassRoomApp.Models.Pupils;
+
+internal class Pupil
+{
+    public virtual void Study()
+    {
+        Console.WriteLine("Method Study() from class Pupil");
+    }
+
+    public virtual void Read()
+    {
+        Console.WriteLine("Method Read() from class Pupil");
+    }
+
+    public virtual void Write()
+    {
+        Console.WriteLine("Method Write() from class Pupil");
+    }
+
+    public virtual void Relax()
+    {
+        Console.WriteLine("Method Relax() from class Pupil");
+    }
+}
---
+++ Essential/003_Inheritance/ClassRoomApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using ClassRoomApp.Models;
+using ClassRoomApp.Models.Pupils;
+
+var firstClass = new ClassRoom(new ExcellentPupil(), new BadPupil());
+
+firstClass.Study();
+firstClass.Write();
+firstClass.Relax();
+firstClass.Read();
+
+Console.ReadKey();
