900f5b5f09c72b794453eb7a922f337ea37d2abd
Kotok Vasily <kottok1998@gmail.com>
2023-06-27 17:05:04 +0300

    refactor(essentialLesson3): reimplementation of DocumentWorker exercise

---
+++ Essential/003_Inheritance/DocumentWorkerApp/Models/ExpertDocumentWorker.cs
@@ -0,0 +1,9 @@
+﻿namespace DocumentWorkerApp.Models;
+
+internal class ExpertDocumentWorker : ProDocumentWorker
+{
+    public override void SaveDocument()
+    {
+        Console.WriteLine("Документ сохранен в новом формате");
+    }
+}
---
+++ Essential/003_Inheritance/DocumentWorkerApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using DocumentWorkerApp.Models;
+
+Console.WriteLine("Для регистрации данного продукта введите лицензионный ключ");
+var key = Console.ReadLine();
+
+DocumentWorker worker;
+
+switch (key)
+{
+    case "PRO":
+        worker = new ProDocumentWorker();
+        break;
+
+    case "EXPERT":
+        worker = new ExpertDocumentWorker();
+        break;
+
+    default:
+        worker = new DocumentWorker();
+        break;
+}
+
+worker.OpenDocument();
+worker.EditDocument();
+worker.SaveDocument();
+
+Console.ReadKey();
