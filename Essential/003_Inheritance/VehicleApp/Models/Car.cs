45bc43ef40637669ca97fb17c88e7cea538d7e22
Kotok Vasily <kottok1998@gmail.com>
2023-06-27 15:34:07 +0300

    refactor(essentialLesson3): reimplementation of Vehicle exercise

---
+++ Essential/003_Inheritance/VehicleApp/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace VehicleApp.Models;
+
+internal class Car : Vehicle
+{
+    public Car(double x, double y, decimal price, double speed, int year)
+        : base(x, y, price, speed, year)
+    {
+    }
+
+    public Car(decimal price, int year)
+        : base(price, year)
+    { 
+    }
+}
---
+++ Essential/003_Inheritance/VehicleApp/Models/Ship.cs
@@ -0,0 +1,18 @@
+﻿namespace VehicleApp.Models;
+
+internal class Ship : Vehicle
+{
+    public int PassengersCount { get; set; }
+
+    public string PortOfResidence { get; set; }
+
+    public Ship(double x, double y, decimal price, double speed, int year)
+    : base(x, y, price, speed, year)
+    {
+    }
+
+    public Ship(decimal price, int year)
+        : base(price, year)
+    {
+    }
+}
