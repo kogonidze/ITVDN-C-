8f0f842a3def3059ebb8488fbbcdeec44dbf289d
Kotok Vasily <kottok1998@gmail.com>
2023-07-10 11:19:33 +0300

    refactor(essentialLesson5): reimplemented WorkWithArrayApp exerice

---
+++ Essential/005_ArraysAndIndexers/WorkWithArrayApp/MyArray.cs
@@ -0,0 +1,84 @@
+﻿namespace WorkWithArrayApp;
+
+internal class MyArray
+{
+    private int[] array;
+
+    public MyArray(int size)
+    {
+        array = new int[size];
+        FillArray();
+    }
+
+    private void FillArray()
+    {
+        var random = new Random();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(int.MaxValue);
+        }
+    }
+
+    public int Max()
+    {
+        int maxValue = int.MinValue;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > maxValue)
+            {
+                maxValue = array[i];
+            }
+        }
+
+        return maxValue;
+    }
+
+    public int Min()
+    {
+        int minValue = int.MaxValue;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] < minValue)
+            {
+                minValue = array[i];
+            }
+        }
+
+        return minValue;
+    }
+
+    public long Sum()
+    {
+        long sum = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            sum += array[i];
+        }
+
+        return sum;
+    }
+
+    public double Average()
+    {
+        var sum = Sum();
+
+        return (double) sum / array.Length;
+    }
+
+    public void PrintOddValues()
+    {
+        Console.WriteLine("Odd values: ");
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 1)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ Essential/005_ArraysAndIndexers/WorkWithArrayApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using WorkWithArrayApp;
+
+var myArray = new MyArray(5);
+Console.WriteLine($"Max: {myArray.Max()}");
+Console.WriteLine($"Min: {myArray.Min()}");
+Console.WriteLine($"Sum: {myArray.Sum()}");
+Console.WriteLine($"Average: {myArray.Average()}");
+myArray.PrintOddValues();
+
+Console.ReadKey();
